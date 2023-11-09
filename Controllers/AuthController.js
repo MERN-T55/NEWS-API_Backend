e7179c5214e75022b08fe1586d4c0bb546bb071f
Charvi Upreti <charviupreti@gmail.com>
2023-11-09 08:11:12 +0530

    first commit

---
+++ Controllers/AuthController.js
@@ -0,0 +1,60 @@
+const User = require("../Models/UserModel");
+const { createSecretToken } = require("../util/SecretToken");
+const bcrypt = require("bcryptjs");
+
+module.exports.Signup = async (req, res, next) => {
+  try {
+    const { email, password, username} = req.body;
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User already exists with same email or username.", success: false });
+    }
+    const user = await User.create({ email, password, username});
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Unable to create user", success: false });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while registering", success: false });
+  }
+};
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({ message: "User does not exist." });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password." });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
